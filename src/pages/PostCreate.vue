9fcd54bf64bf56b77b010992b4ea04e06f2fb095
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-11 13:04:01 +0200

    add > post create html css structure

--- src/pages/PostCreate.vue
+++ src/pages/PostCreate.vue
@@ -1,14 +1,84 @@
-<script>
-export default {
-    
-}
-</script>
-
 <template>
-    <div>
-        post create
+    <div class="create-post">
+      <h1>Crea un nuovo post</h1>
+      <form class="post-form">
+        <div class="form-group">
+          <label for="title">Titolo:</label>
+          <input type="text" id="title" v-model="title" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="image">Immagine:</label>
+          <input type="text" id="image" v-model="image" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="content">Contenuto:</label>
+          <textarea id="content" v-model="content" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="category">Categoria:</label>
+          <select id="category" v-model="categoryId" class="form-control">
+            <!-- ciclo for per le categorie -->
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="tags">Tags:</label>
+          <input type="text" id="tags" v-model="tags" class="form-control">
+        </div>
+        <button type="submit">Crea Post</button>
+      </form>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        title: '',
+        image: '',
+        content: '',
+        categoryId: null,
+        tags: ''
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .create-post {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  
+  .post-form {
+    background-color: #f4f4f4;
+    padding: 20px;
+    border-radius: 5px;
+  }
+  
+  .form-group {
+    margin-bottom: 20px;
+    color: black;
+  }
 
-<style scoped>
-</style>
+  .form-group > input,
+  .form-group > select,
+  .form-group > textarea {
+    background-color: white;
+  }
+  
+  .label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  .form-control {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  </style>
+  
